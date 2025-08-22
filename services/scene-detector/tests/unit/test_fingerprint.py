cc6963db2c23ed3ab288b0b186a02afd1178ace2
Antonio Feregrino <antonio.feregrino@gmail.com>
2025-08-22 18:28:19 +0100

    Fix fingerprinting tests

--- services/scene-detector/tests/unit/test_fingerprint.py
+++ services/scene-detector/tests/unit/test_fingerprint.py
@@ -6,16 +6,21 @@ from scene_detector.fingerprint import compute_fingerprint, fingerprint_distance
 def test_compute_fingerprint(image_as_array):
     frame = image_as_array("frame_560.png")
     fingerprint = compute_fingerprint(frame)
-    assert fingerprint == "c4694538a522b592f294d32b9bd9cdf4d9b8e8cd9c5ab8a9d404da2ed216f546"
+    assert fingerprint == (
+        "fff807fffff803ff01fc00ff00fc00f0287e00f0003e0070001f8000023f8000"
+        "03ffe00007fff80001fffc000efffc000ffffe000ffffe003fffff008c7fff00"
+        "6b39fdc0403bffc0000fdf98000fff8800ff1fbc208f9ff401f9d7e201f87773"
+        "07fc247117ce08780bfe00fc1ffe18163ffe1807bffe0c07bffed8011f7e7a09"
+    )
 
 
 @pytest.mark.parametrize(
     "image_one, image_two, expected_distance",
     [
         ("frame_560.png", "frame_560.png", 0),
-        ("frame_560.png", "frame_561.png", 2),
-        ("frame_0.png", "frame_10.png", 128),
-        ("frame_0.png", "frame_1000.png", 128),
+        ("frame_560.png", "frame_561.png", 4),
+        ("frame_0.png", "frame_10.png", 486),
+        ("frame_0.png", "frame_1000.png", 511),
     ],
 )
 def test_fingerprint_distance_exact_match(image_as_array, image_one, image_two, expected_distance):
